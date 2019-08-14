edb1f070fee727c9a1ed4813f5596de901a15604
josh <48034855+JDJoffe@users.noreply.github.com>
2019-08-14 13:51:00 +1000

    enemy spawn + sync

--- Assets/Scripts/CustomNetworkManager.cs
+++ Assets/Scripts/CustomNetworkManager.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 using Mirror;
 public class CustomNetworkManager : NetworkManager
 {
-    public override void OnServerConnect(NetworkConnection conn)
-    {
-        base.OnServerConnect(conn);
+    //public override void OnServerConnect(NetworkConnection conn)
+    //{
+    //    base.OnServerConnect(conn);
 
-        //spawn instance of enemy on client
-        GameObject enemyPrefab = spawnPrefabs[0];
-        GameObject enemy = Instantiate(enemyPrefab, new Vector3(0,1,0),Quaternion.identity);
-        NetworkServer.Spawn(enemy);
-    }
+    //    //spawn instance of enemy on client
+    //    GameObject enemyPrefab = spawnPrefabs[0];
+    //    GameObject enemy = Instantiate(enemyPrefab, new Vector3(0,1,0),Quaternion.identity);
+    //    NetworkServer.Spawn(enemy);
+    //}
 }
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class EnemySpawner : NetworkBehaviour
+{
+    public GameObject enemyPrefab;
+    public float spawnTimer = 1f;
+
+    // Start is called before the first frame update
+    public override void OnStartServer()
+    {
+        InvokeRepeating("SpawnEnemy", this.spawnTimer, this.spawnTimer);
+    }
+
+   void SpawnEnemy()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-4f, 4f), this.transform.position.y, Random.Range(-4f,4f));
+        GameObject enemy = Instantiate(enemyPrefab, spawnPos, Quaternion.identity) as GameObject;
+        NetworkServer.Spawn(enemy);
+        Destroy(enemy, 10);
+    }
+}

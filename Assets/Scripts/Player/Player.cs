d0aa07b21942540424c6f22bb9b7473420ad2891
josh <48034855+JDJoffe@users.noreply.github.com>
2019-08-14 10:12:41 +1000

    initial

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f, jump = 10f;
+    public LayerMask ignoreLayers;
+    public float rayDistance = 10f;
+    public bool isGrounded = false;
+    private Rigidbody rigid;
+    #region Unity Events
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * rayDistance);
+    }
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        Ray groundRay = new Ray(transform.position, Vector3.down);
+        isGrounded = Physics.Raycast(groundRay, rayDistance, ~ignoreLayers);
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        Item item = col.GetComponent<Item>();
+        if (item)
+        {
+            item.Collect();
+        }
+    }
+    private void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+        Move(inputH, inputV);
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+    }
+    #endregion
+    #region Custom
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            rigid.AddForce(Vector3.up * jump, ForceMode.Impulse);
+        }
+    }
+    private void Move(float inputH, float inputV)
+    {
+        Vector3 direction = new Vector3(inputH, 0, inputV);
+
+        // [Optional] Move with camera
+        Vector3 euler = Camera.main.transform.eulerAngles;
+        direction = Quaternion.Euler(0, euler.y, 0) * direction; // Convert direction to relative direction to camera only on Y
+
+        rigid.AddForce(direction * speed);
+    }
+    #endregion
+}
+

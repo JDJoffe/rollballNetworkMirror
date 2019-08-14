8d8b530908b57a797f702836e175b0175ab25d97
josh <48034855+JDJoffe@users.noreply.github.com>
2019-08-14 16:00:49 +1000

    Update Bomb.cs

--- Assets/Scripts/Entities/Bomb.cs
+++ Assets/Scripts/Entities/Bomb.cs
@@ -5,11 +5,11 @@ using Mirror;
 public class Bomb : NetworkBehaviour
 {
     public float explosionRadius = 2f;
-
+    public float bombTimer = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Explode());
     }
     private void OnDrawGizmos()
     {
@@ -19,6 +19,20 @@ public class Bomb : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public IEnumerator Explode()
+    {
+        yield return new WaitForSeconds(bombTimer);
+        CmdExplode(transform.position, explosionRadius);
+    }
+    [Command]
+    void CmdExplode(Vector3 pos, float rad)
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (var hit in hits)
+        {
+            NetworkServer.Destroy(hit.gameObject);
+        }
     }
 }
